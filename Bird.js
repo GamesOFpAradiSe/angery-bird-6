1b1eccbc48e8e945dde344f81b4610bf13f73b28
GamesOFpAradiSe <minua952@gmail.com>
2021-06-24 17:09:07 +0530

    angery bird 6

--- Bird.js
+++ Bird.js
@@ -2,11 +2,25 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.img2 = loadImage("sprites/smoke.png")
+    this.arr = []
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
+    if(this.body.position.x > 220 && this.body.velocity.x>1){
+    var position = [this.body.position.x,this.body.position.y]
+    this.arr.push(position)
+    }
+    for(var i=0; i<this.arr.length;i++){
+
+image(this.img2,this.arr[i][0],this.arr[i][1])
+
+
+    }
+
     super.display();
   }
-}
+  }
+
